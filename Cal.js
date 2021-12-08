d1580acc2aadd9a9a297e65a338d8ce2a4202c12
Karan Meena <87779391+karanmeena237@users.noreply.github.com>
2021-12-08 14:56:09 +0530

    Add files via upload

---
+++ Cal.js
@@ -0,0 +1,56 @@
+var button = document.getElementsByClassName("bt")
+var display = document.getElementsByClassName("display")
+[0] //if got an error for trim()
+// display.textContent = 0;
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
+function isOperator(value){
+    return value == "+" || value == "-" || value == "*" || value == "/";
+}
+
+for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function () {
+
+        var value = this.getAttribute('data-value');
+        var text = display.textContent.trim();
+    if(isOperator(value)){
+        operator = value;
+        operand1 = parseFloat(text);
+        display.textContent="";
+    }
+    else if(value == "%"){
+        operand1 = parseFloat(text);
+        operand1 /= 100;
+        display.textContent = operand1;
+    }
+    else if(value =="ac"){
+        display.textContent="";
+    }
+    else if(value == "sign"){
+        operand1 = parseFloat(text);
+        operand1 = -1 * operand1;
+        display.textContent = operand1;
+    }
+    else if(value == "."){
+        if(text.length && !text.includes('.')){
+            display.textContent = text + '.';
+        }
+    }
+    else if ( value == "="){
+        console.log("presssed");
+        operand2 = parseFloat(text);
+        var result = eval(operand1 + operator + operand2);
+        console.log(result);
+        if(result){
+            display.textContent = result;
+            operand1 = result;
+            operand2 = null;
+            operator = null;
+        }
+    }
+    else{
+        display.textContent += value;
+    }
+});
+}
